cab8b643e1992dab65c834d40b34b05a1f340d57
vanshika-21 <vanshikajain238@gmail.com>
2023-09-11 00:25:36 +0530

    completed timestamps selection functionality

--- src/Components/ShowPage.js
+++ src/Components/ShowPage.js
@@ -3,31 +3,94 @@ import { useParams } from "react-router-dom";
 import compliancetaskdata from "../compliance-task-data.json";
 
 function ShowPage() {
-  const { episodeNumber } = useParams(); // Get episodeNumber from the URL
+  const { episodeNumber } = useParams();
   const [episodeData, setEpisodeData] = useState(null);
+  const [checkedItems, setCheckedItems] = useState({});
+  const [showStatus, setShowStatus] = useState("incomplete");
 
   useEffect(() => {
-    // Find the episode data in the JSON file based on the episodeNumber
     const selectedEpisode = compliancetaskdata.find(
       (episode) => episode.episodeNumber === episodeNumber
     );
 
     if (selectedEpisode) {
-      // Set the episode data if it's found
       setEpisodeData(selectedEpisode);
+      initializeCheckedItems(selectedEpisode.items);
+      setShowStatus(selectedEpisode.status);
     } else {
-      // Handle the case where the episode data is not found
-      // For example, you can display a message or redirect to an error page
+      return <p>Episode not found</p>;
     }
   }, [episodeNumber]);
+
+  useEffect(() => {
+    // Check if all items are checked
+    const areAllItemsChecked = Object.values(checkedItems).every(
+      (isChecked) => isChecked
+    );
+
+    // Enable the "Complete" button only if all items are checked
+    setShowStatus(areAllItemsChecked ? "completed" : "incomplete");
+    console.log("current value", showStatus);
+  }, [checkedItems]);
+
+  const initializeCheckedItems = (items) => {
+    const initialCheckedItems = {};
+
+    items.forEach((item, index) => {
+      initialCheckedItems[index] = false; // Initialize all items as unchecked
+    });
+
+    setCheckedItems(initialCheckedItems);
+  };
+
+  const handleCheckboxChange = (index) => {
+    setCheckedItems({
+      ...checkedItems,
+      [index]: !checkedItems[index],
+    });
+    console.log(showStatus);
+  };
+
+  const handleCompleteButtonClick = () => {
+    if (showStatus === "completed") {
+      alert("Show marked as completed!");
+    }
+  };
+
+  // console.log(episodeData);
 
   return (
     <div>
       {episodeData ? (
         <>
           <h2>Episode Number: {episodeData.episodeNumber}</h2>
-          {/* Display other episode details as needed */}
           <h2>Show name: {episodeData.show}</h2>
+          <h2>Show Status: {episodeData.status}</h2>
+          <h2>Items:</h2>
+          {episodeData.status === "Completed"
+            ? episodeData.items.map((item, index) => (
+                <div key={index}>{item.comment}</div>
+              ))
+            : episodeData.items.map((item, index) => (
+                <div key={index}>
+                  <input
+                    type="checkbox"
+                    checked={checkedItems[index]}
+                    onChange={() => handleCheckboxChange(index)}
+                  />
+                  {item.comment}
+                </div>
+              ))}
+          {episodeData.status !== "Completed" && (
+            <>
+              <button
+                disabled={showStatus === "incomplete"}
+                onClick={handleCompleteButtonClick}
+              >
+                Complete
+              </button>
+            </>
+          )}
         </>
       ) : (
         <p>Episode not found</p>
