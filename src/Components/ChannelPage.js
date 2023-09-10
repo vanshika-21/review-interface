8db731f7ddefcc9418478b866bd8960e5c9ea7f1
vanshika-21 <vanshikajain238@gmail.com>
2023-09-10 15:44:55 +0530

    added search box filtering functionality to the channel page

--- src/Components/ChannelPage.js
+++ src/Components/ChannelPage.js
@@ -6,6 +6,8 @@ function ChannelPage() {
   const { channelName } = useParams();
   const [data, setData] = useState([]);
   const [channels, setChannels] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredShows, setFilteredShows] = useState([]);
 
   const getChannels = (data) => {
     const filteredData = data.filter(
@@ -16,6 +18,27 @@ function ChannelPage() {
     return filteredData;
   };
 
+  const updateFilteredShows = (query) => {
+    if (!query) {
+      return getChannels(data); // Return all shows when query is empty
+    }
+
+    const filtered = getChannels(data).filter((episode) =>
+      episode.show.toLowerCase().startsWith(query.toLowerCase())
+    );
+    return filtered;
+  };
+
+  const handleInputChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+
+    if (!query) {
+      setFilteredShows(getChannels(data)); // Set all shows when query is empty
+    } else {
+      setFilteredShows(updateFilteredShows(query));
+    }
+  };
   // const getData = () => {
   //   fetch("compliance-task-data.json")
   //     .then((response) => response.json())
@@ -48,16 +71,36 @@ function ChannelPage() {
 
   useEffect(() => {
     setChannels(getChannels(compliancetaskdata));
+    setData(compliancetaskdata);
+    setFilteredShows(getChannels(compliancetaskdata));
     console.log(compliancetaskdata);
   }, []);
 
   return (
     <div>
       <h1>channel name : {channelName} </h1>
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search shows..."
+          className="search-input"
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
+      </div>
       <>
-        {channels.map((data) => {
+        {/* {channels.map((data) => {
           const showLink = `/channel/${channelName}/show/${data.episodeNumber}`;
           // return <div>Show name: {data.show}</div>;
+          return (
+            <div key={data.episodeNumber}>
+              <Link to={showLink}>{data.show}</Link>
+            </div>
+          );
+        })} */}
+
+        {filteredShows.map((data) => {
+          const showLink = `/channel/${channelName}/show/${data.episodeNumber}`;
           return (
             <div key={data.episodeNumber}>
               <Link to={showLink}>{data.show}</Link>
